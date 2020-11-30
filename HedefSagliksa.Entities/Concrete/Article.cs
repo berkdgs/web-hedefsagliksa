b32d0709838f392a0a376cd8d28ad7225322d2be
İbrahim Doğuş <idogus@gmail.com>
2020-11-30 20:29:50 +0300

    Add one entity

---
+++ HedefSagliksa.Entities/Concrete/Article.cs
@@ -0,0 +1,13 @@
+﻿using HedefSagliksa.Entities.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HedefSagliksa.Entities.Concrete
+{
+    public class Article : IEntity
+    {
+    }
+}

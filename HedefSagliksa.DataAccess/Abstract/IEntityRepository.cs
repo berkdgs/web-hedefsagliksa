c164f6d7923fa5d36227d7f599c7a5a6a6fa7a65
İbrahim Doğuş <42433448+idogus@users.noreply.github.com>
2020-12-13 19:08:16 +0300

    Add files via upload

--- HedefSagliksa.DataAccess/Abstract/IEntityRepository.cs
+++ HedefSagliksa.DataAccess/Abstract/IEntityRepository.cs
@@ -12,6 +12,7 @@ namespace HedefSagliksa.DataAccess.Abstract
     {
         List<T> GetAll(Expression<Func<T, bool>> filter = null);
         T Get(Expression<Func<T, bool>> filter);
+        T GetById(string id);
         void Add(T entity);
         void Update(T entity);
         void Delete(T entity);
